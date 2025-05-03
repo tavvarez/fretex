a9db582a5c19b9b5f81a1e252dd02f87b972a086
tavvarez <gabrieldaraujotavares@gmail.com>
2025-05-03 10:08:03 -0300

    análise com matplotlib

--- etl/analise_exploratoria.py
+++ etl/analise_exploratoria.py
@@ -2,10 +2,12 @@ import pandas as pd;
 from db_connection import get_engine;
 import matplotlib.pyplot as plt;
 import seaborn as sns;
+from dotenv import load_dotenv;
+import os;
 
 engine = get_engine();
 
-query = "SELECT * FROM fretes";
+query = os.getenv('QUERY');
 df = pd.read_sql(query, engine);
 
 print("Total de fretes: ", len(df));
@@ -33,3 +35,23 @@ plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=["#4CAF50
 plt.title('Entrega no prazo vs Atrasadas');
 plt.axis('equal');
 plt.show()
+
+frete_medio_dois = df.groupby('tipo_veiculo')['valor_frete'].mean().sort_values();
+
+plt.figure(figsize=(8,5));
+sns.barplot(x=frete_medio_dois.index, y=frete_medio_dois.values, palette="Blues_d");
+plt.title('Frete médio p/ veículo');
+plt.xlabel('tipo de veículo');
+plt.xlabel('valor médio do frete em reais');
+plt.xticks(rotation = 45);
+plt.show();
+
+top_transportadoras = df['transportadora'].value_counts().head(5);
+
+plt.figure(figsize=(8,5));
+sns.barplot(x=top_transportadoras.index, y=top_transportadoras.values, palette="Greens_d");
+plt.title('Top 5 Transportadoras');
+plt.xlabel('Transportadora');
+plt.ylabel('Quantidade de Fretes');
+plt.xticks(rotation=45);
+plt.show();
