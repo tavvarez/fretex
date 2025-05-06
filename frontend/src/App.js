4ef3263732d7e587bfbf3003b779cdcf407ebc9f
tavvarez <gabrieldaraujotavares@gmail.com>
2025-05-05 22:26:13 -0300

    prod

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
     setLoading(true);
     setSnackbarOpen(true);
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://fretex-api-c5ezcebebshaaxbu.eastus2-01.azurewebsites.net/predict', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
